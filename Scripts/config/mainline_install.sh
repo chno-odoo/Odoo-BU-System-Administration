374ef4b2da62089a399cd5af3a3570f02d5b8b68
Christopher Nowak <chno@odoo.com>
2024-10-16 14:49:18 -0400

    [FIX] Added install of software properties as this is a requirement to add the repo.

--- Scripts/config/mainline_install.sh
+++ Scripts/config/mainline_install.sh
@@ -2,8 +2,14 @@
 ### Script to install mainline kernel manager ###
 # Written by: chno
 # Creation Date: Wed Oct 16 02:33:32 PM EDT 2024
-# Last Update:Wed Oct 16 02:33:32 PM EDT 2024
-# Last Update:N/A
+# Last Update:Wed Oct 16 02:46:59 PM EDT 2024
+# Last Update:Added the install of software-properties-common as this is where the add-apt-repository command comes from and it was not installed.
+
+echo "Updating packages..."
+sudo apt update && sudo apt upgrade -y
+
+echo "Installing software properties..."
+sudo apt install software-properties-common
 
 echo "Adding repo..."
 sudo add-apt-repository ppa:cappelikan/ppa -y
