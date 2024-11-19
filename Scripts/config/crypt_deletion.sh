113ddf9fc82f8daec7e1bcef2568f645592cf395
Christopher Nowak <chno@odoo.com>
2024-11-19 13:04:14 -0500

    Fix

--- Scripts/config/crypt_deletion.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# A script to remove a crypt key.
-
-sudo cryptsetup luksRemoveKey /dev/nvme0n1p3
--- Scripts/custos/flameshot_use.sh
+++
@@ -1,4 +0,0 @@
-echo '#!/bin/bash' > ~/flameshot-launcher.sh
-echo 'xhost +local:' >> ~/flameshot-launcher.sh
-echo '/usr/bin/flameshot gui' >> ~/flameshot-launcher.sh
-chmod +x ~/flameshot-launcher.sh
